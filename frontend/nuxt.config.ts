340fe812d3edbeed5e7b808b27f40bc3d33a1cfe
y.abdilkakov <y.abdilkakov@fdrive.kz>
2026-03-06 20:46:45 +0500

    corsdd

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8000',
+      apiBase: 'https://api-toi.daraedu.kz',
     },
   },
 

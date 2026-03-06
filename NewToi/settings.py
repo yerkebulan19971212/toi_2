1e9520364feb4165acbc53dbeff4ab07828bd5da
y.abdilkakov <y.abdilkakov@fdrive.kz>
2026-03-06 17:53:55 +0500

    run v2ddddec

--- NewToi/settings.py
+++ NewToi/settings.py
@@ -118,7 +118,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS
 CORS_ALLOWED_ORIGINS = config(
     'CORS_ALLOWED_ORIGINS',
-    default='http://localhost:3000,http://127.0.0.1:3000',
+    default='http://localhost:3000,http://127.0.0.1:3000,http://api-toi.daraedu.kz,http://toi.daraedu.kz',
     cast=lambda v: [s.strip() for s in v.split(',')]
 )
 CORS_ALLOW_CREDENTIALS = True

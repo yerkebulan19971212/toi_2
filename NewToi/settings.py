1fc2aa2401ce686b0115706f8f700cf880dbc1aa
y.abdilkakov <y.abdilkakov@fdrive.kz>
2026-03-06 16:31:47 +0500

    sett

--- NewToi/settings.py
+++ NewToi/settings.py
@@ -19,11 +19,12 @@ SECRET_KEY = config(
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config(
-    'ALLOWED_HOSTS',
-    default='localhost,127.0.0.1',
-    cast=lambda v: [s.strip() for s in v.split(',')]
-)
+# ALLOWED_HOSTS = config(
+#     'ALLOWED_HOSTS',
+#     default='localhost,127.0.0.1',
+#     cast=lambda v: [s.strip() for s in v.split(',')]
+# )
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',

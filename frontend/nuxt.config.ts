43b0fdf0b382b1e6e00d95b89c6e1e6bca6abbe3
y.abdilkakov <y.abdilkakov@fdrive.kz>
2026-05-10 13:49:02 +0500

    local: update apiBase and remove unnecessary references

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -11,9 +11,9 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
 
   runtimeConfig: {
-    apiBase: process.env.NUXT_API_BASE || '',
+    apiBase: '',
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8000',
+      apiBase: 'http://localhost:8000',
     },
   },
 
